4bca8487c5af9e1bea31115b47d2420d8a3fc3fd
x-Xymos <xymos0@gmail.com>
2019-07-28 00:17:02 +0100

    explored different models, improved the accuracy

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import Imputer
 from sklearn.impute import SimpleImputer
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import *
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-data = pd.read_csv('iowaHomes.csv')
-
-#according to the info.txt file NA = NO for most categories, for example no fireplace,
-#no basement, does that mean missing values = NO???
 
 def replace_null(x):
     if pd.isnull(x):
@@ -31,6 +19,14 @@ def replace_null(x):
     else:
         return x
 
+def factorize_data(d):
+    for x in range(d.shape[1]):
+        if d.iloc[:, x].dtype == "object":
+            d.iloc[:, x] = pd.factorize(d.iloc[:, x])[0]
+
+    return d
+
+data = pd.read_csv('iowaHomes.csv')
 
 #replacing null categorical values
 data['FireplaceQu'] = data['FireplaceQu'].map(replace_null)
@@ -40,126 +36,170 @@ data['BsmtExposure'] = data['BsmtExposure'].map(replace_null)
 data['BsmtFinType1'] = data['BsmtFinType1'].map(replace_null)
 data['BsmtFinType2'] = data['BsmtFinType2'].map(replace_null)
 data['GarageType'] = data['GarageType'].map(replace_null)
-#??? combine basement attributes into basement or no basement ???
 data['MasVnrType'] = data['MasVnrType'].map(lambda x: "None" if pd.isnull(x) else x)
 data['Electrical'] = data['Electrical'].map(lambda x: "SBrkr" if pd.isnull(x) else x)
 data['GarageQual'] = data['GarageQual'].map(replace_null)
 data['GarageCond'] = data['GarageCond'].map(replace_null)
 data['GarageFinish'] = data['GarageFinish'].map(replace_null)
 data['GarageType'] = data['GarageType'].map(replace_null)
-
 data['PoolQC'] = data['PoolQC'].map(replace_null)
 data['Fence'] = data['Fence'].map(replace_null)
 data['MiscFeature'] = data['MiscFeature'].map(replace_null)
 
 #replacing null numeric values
-imputer = SimpleImputer(missing_values=np.nan, strategy='median')
-data[['LotFrontage']] = imputer.fit_transform(data[['LotFrontage']])
 data['MasVnrArea'] = data['MasVnrArea'].map(lambda x: 0 if pd.isnull(x) else x)
-data[['GarageYrBlt']] = imputer.fit_transform(data[['GarageYrBlt']])
-
-#??? combine garage attributes into garage or no garage ???
 
 
 #print(data.isnull().sum())
-#print(data['Alley'].value_counts())
+#print(data['MasVnrArea'].value_counts())
 
-#todo compare model performance
-# with and without replaced data
+data = data.drop(columns=["Id", "Alley",
+                          "PoolArea",
+                          "MoSold",
+                          "3SsnPorch",
+                          "BsmtFinSF2",
+                          "BsmtHalfBath",
+                          "MiscVal",
+                          "LowQualFinSF",
+                          "YrSold",
+                          "OverallCond",
+                          "ScreenPorch"])
+#dropping a lot of attributes with low correlation to reduce noise
+#seems to have improved the variance score a little bit
 
-data = data.drop(columns=["Id", "Alley"])
-
-corr_matrix = data.corr()
+#corr_matrix = data.corr()
 #print(corr_matrix["SalePrice"].sort_values(ascending=False))
 
-def factorize_data(d):
-    for x in range(d.shape[1]):
-        if d.iloc[:, x].dtype == "object":
-            d.iloc[:, x] = pd.factorize(d.iloc[:, x])[0]
-
-    return data
-
-data['MSSubClass'] = pd.factorize(data['MSSubClass'])[0]
-
-dataNorm = data.drop(columns=["SalePrice", "YrSold", "MoSold",
-                              "GarageYrBlt", "YearRemodAdd",
-                              "YearBuilt", "MSSubClass"
-                              ])
-
-
-dataNorm = dataNorm.select_dtypes(include=[np.number])
-dataNorm_col = dataNorm.select_dtypes(include=[np.number]).columns
 
 #trying out different data scaling functions
 distributions = [
-    ('Unscaled data', dataNorm),
-    ('Data after standard scaling',
-        StandardScaler().fit_transform(dataNorm)),
+    ('Unscaled data', "NA"),
     ('Data after min-max scaling',
-        MinMaxScaler().fit_transform(dataNorm)),
+        MinMaxScaler().fit_transform),
     ('Data after max-abs scaling',
-        MaxAbsScaler().fit_transform(dataNorm)),
+        MaxAbsScaler().fit_transform),
     ('Data after robust scaling',
-        RobustScaler(quantile_range=(25, 75)).fit_transform(dataNorm)),
+        RobustScaler(quantile_range=(25, 75)).fit_transform),
     ('Data after power transformation (Yeo-Johnson)',
-     PowerTransformer(method='yeo-johnson').fit_transform(dataNorm)),
+     PowerTransformer(method='yeo-johnson').fit_transform),
     ('Data after power transformation (Box-Cox)',
-     PowerTransformer().fit_transform(dataNorm)),
-    ('Data after quantile transformation (gaussian pdf)',
-        QuantileTransformer(output_distribution='normal')
-        .fit_transform(dataNorm)),
+     PowerTransformer().fit_transform),
     ('Data after quantile transformation (uniform pdf)',
         QuantileTransformer(output_distribution='uniform')
-        .fit_transform(dataNorm)),
+        .fit_transform),
     ('Data after sample-wise L2 normalizing',
-        Normalizer().fit_transform(dataNorm)),
+        Normalizer().fit_transform),
+    ('Data after standardization',
+     StandardScaler().fit_transform)
 ]
 
 for dist in distributions:
+    cleanData = data.copy()
+
+
+    dataNorm = cleanData.drop(columns=["SalePrice",
+                                  "GarageYrBlt", "YearRemodAdd",
+                                  "YearBuilt", "MSSubClass", "OverallQual",
+                                  "BsmtFullBath", 'FullBath', 'HalfBath',
+                                  "BedroomAbvGr", "KitchenAbvGr", "TotRmsAbvGrd",
+                                  "Fireplaces", "GarageCars"
+                                  ])
+    dataNorm = dataNorm.select_dtypes(include=[np.number])
+    dataNorm_col = dataNorm.select_dtypes(include=[np.number]).columns
+
+
+    if dist[1] != "NA":
+        try:
+            dataNorm = dist[1](dataNorm)
+            cleanData[dataNorm_col] = dataNorm
+        except Exception as exc:
+            print(exc)
+            continue
+
+    cleanData = factorize_data(cleanData)
+
+    from sklearn.model_selection import KFold
+
+    scores = []
+
+    kf = KFold(n_splits=10, random_state=42)
+    for train, test in kf.split(cleanData):
+
+        train_data = cleanData.drop(columns='SalePrice').iloc[train]
+        train_labels = cleanData['SalePrice'].iloc[train]
+
+        test_data = cleanData.drop(columns='SalePrice').iloc[test]
+        test_labels = cleanData['SalePrice'].iloc[test]
+
+        imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+
+        train_data[['LotFrontage']] = imputer.fit_transform(train_data[['LotFrontage']])
+        train_data[['GarageYrBlt']] = imputer.fit_transform(train_data[['GarageYrBlt']])
+
+        test_data[['LotFrontage']] = imputer.fit_transform(test_data[['LotFrontage']])
+        test_data[['GarageYrBlt']] = imputer.fit_transform(test_data[['GarageYrBlt']])
+
+        #linear regression
+        # reg = linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=-1,
+        #                  normalize=False)
+        # reg.fit(train_data, train_labels)
+        # pred = reg.predict(test_data)
+        # print(pred[1],test_labels.iloc[1])
+        # acc = explained_variance_score(test_labels, pred)
+        # scores.append(acc)
+        ##########################################################################
+
+        #random forest regressor
+
+        from sklearn.model_selection import GridSearchCV
+
+        param_grid = [
+            {'n_estimators': [50, 100, 150, 200], 'max_features': [4, 8, 32, 64], 'n_jobs':[-1]},
+            {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+        ]
+
+
+        from sklearn.ensemble import RandomForestRegressor
+        reg = RandomForestRegressor(n_estimators=100, max_features=32, random_state=42, n_jobs=-1)
+
+        #grid_search = GridSearchCV(reg, param_grid, cv=5,scoring='r2')
+
+        reg.fit(train_data, train_labels)
+        #print(grid_search.best_estimator_)
+        pred = reg.predict(test_data)
+        #print(pred[3],test_labels.iloc[3])
+        scores.append(explained_variance_score(test_labels, pred))
+        ##########################################################################
+
+        # #############################################################################
+        # Lasso
+        # from sklearn.linear_model import Lasso
+        #
+        # alpha = 0.1
+        # lasso = Lasso(alpha=alpha,max_iter=5000, random_state=42)
+        #
+        # y_pred_lasso = lasso.fit(train_data, train_labels).predict(test_data)
+        # r2_score_lasso = r2_score(test_labels, y_pred_lasso)
+        # scores.append(r2_score_lasso)
+
+        # #############################################################################
+
+
+        #lars
+        # from sklearn import linear_model
+        #
+        # reg = linear_model.Lars(n_nonzero_coefs=25)
+        # y_pred = reg.fit(train_data, train_labels).predict(test_data)
+        # r2_score_sgd = r2_score(test_labels, y_pred)
+        # scores.append(r2_score_sgd)
+        #############################3
+
     print(dist[0])
-    dataNorm = dist[1]
-
-#dataNorm = preprocessing.QuantileTransformer(output_distribution="normal").fit_transform(dataNorm)
-
-    data[dataNorm_col] = dataNorm
-
-    data = factorize_data(data)
-
-    #print(data)
-
-
-    y = data['SalePrice']
-    x = data.drop(columns='SalePrice')
-
-
-    train_data, test_data, train_labels, test_labels = train_test_split(x, y, test_size=0.3, random_state=42)
-
-    reg = linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=-1,
-                     normalize=False)
-    reg.fit(train_data, train_labels)
-
-    pred = reg.predict(test_data)
-    acc = explained_variance_score(test_labels, pred)
-
-    print(acc)
+    print(np.mean(scores))
 
 
 
 
-
-
-
-#print(data.info())
-# pca = PCA().fit(data)
-# print('Explained variance by component: %s' % pca.explained_variance_ratio_)
-# print(pd.DataFrame(pca.components_, columns=data.feature_names))
-
-#print(data.info())
-
-#data.iloc[:, 1:]
-
-#y = data.iloc[:, 0]
-#x = data.drop(columns=1)
 
 
 
