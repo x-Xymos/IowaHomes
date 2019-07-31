025c139021478542923dd05881b13cc1933aa4bb
Xymos <xymos0@gmail.com>
2019-07-31 16:49:53 +0100

    31-07-2019

--- main.py
+++ main.py
@@ -36,20 +36,22 @@ def linear_reg(train_data, train_labels, test_data, test_labels,
     print("RMSLE ", mean_squared_log_error(test_labels, pred))
     print("RMSE ", mean_squared_error(test_labels, pred))
     print("Variance ", explained_variance_score(test_labels, pred))
-    print("R2 ", r2_score(test_labels, pred))
 
 
     if pred_set is not None:
         v_pred = reg.predict(pred_set)
         v_pred = np.exp(v_pred)
         res = pd.DataFrame({"Id": pred_set_id, "SalePrice": v_pred})
-        res.to_csv("predictions.csv", index=False)
+        res.to_csv("predictionsLinearReg.csv", index=False)
+
+    return pred, v_pred
+
 
 def lasso(train_data, train_labels, test_data, test_labels,
                      pred_set=None,pred_set_id=None):
 
     from sklearn.model_selection import RandomizedSearchCV  # Number of trees in random forest
-    alpha = [x for x in np.arange(start=0.0, stop=1, step=0.01)]
+    cv = [x for x in np.arange(start=2, stop=15, step=1)]
     # Number of features to consider at every split
     fit_intercept = [True, False]
     # Maximum number of levels in tree
@@ -61,20 +63,19 @@ def lasso(train_data, train_labels, test_data, test_labels,
     tol = [x for x in np.arange(start=0.0, stop=1, step=0.01)]
     # Method of selecting samples for training each tree
     positive = [True, False]  # Create the random grid
-    random_grid = {'alpha': alpha,
+    random_grid = {'cv': cv,
                    'fit_intercept': fit_intercept,
                    'normalize': normalize,
                    'precompute': precompute,
                    'max_iter': max_iter,
                    'tol': tol}
-
-    reg = linear_model.Lasso(tol=0.04, precompute=True, normalize=True,
-                             max_iter=200,fit_intercept=True, alpha=0.0,
+    #reg = linear_model.LassoCV()
+    reg = linear_model.LassoCV(tol=0.00, precompute=True, normalize=True,
+                             max_iter=300,fit_intercept=True, cv=13,
                              random_state=42)
-
-    #rf_random = RandomizedSearchCV(estimator=reg, param_distributions=random_grid, n_iter=1000, cv=3, verbose=2,random_state=42, n_jobs=-1)  # Fit the random search model
-    #rf_random.fit(train_data, train_labels)
-    #print(rf_random.best_params_)
+    # rf_random = RandomizedSearchCV(estimator=reg, param_distributions=random_grid, n_iter=1000, cv=3, verbose=2,random_state=42, n_jobs=-1)  # Fit the random search model
+    # rf_random.fit(train_data, train_labels)
+    # print(rf_random.best_params_)
 
     reg.fit(train_data, train_labels)
 
@@ -84,80 +85,60 @@ def lasso(train_data, train_labels, test_data, test_labels,
     print("RMSLE ", mean_squared_log_error(test_labels, pred))
     print("RMSE ", mean_squared_error(test_labels, pred))
     print("Variance ", explained_variance_score(test_labels, pred))
-    print("R2 ", r2_score(test_labels, pred))
-
-    # print("Validation Set")
-    # pred = reg.predict(valid_set_data)
-    # print(mean_squared_log_error(valid_set_labels, pred))
-    # print(explained_variance_score(valid_set_labels, pred))
 
     if pred_set is not None:
         v_pred = reg.predict(pred_set)
         v_pred = np.exp(v_pred)
         res = pd.DataFrame({"Id": pred_set_id, "SalePrice": v_pred})
-        res.to_csv("predictions.csv", index=False)
+        res.to_csv("predictionsLasso.csv", index=False)
+
+    return pred, v_pred
 
 
-def forest_regressor(train_data, train_labels, test_data, test_labels,
+def bayesian_ridge(train_data, train_labels, test_data, test_labels,
                      pred_set=None,pred_set_id=None):
-    # random forest regressor
-    from sklearn.model_selection import GridSearchCV
 
-    from sklearn.model_selection import RandomizedSearchCV  # Number of trees in random forest
-    n_estimators = [int(x) for x in np.linspace(start=10, stop=2000, num=10)]
-    max_features = ['auto', 'sqrt','log2', None]
-    max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-    max_depth.append(None)
-    min_samples_split = [2, 5, 10]
-    min_samples_leaf = [1, 2, 4]
-    min_weight_fraction_leaf = [x for x in np.arange(start=0.0, stop=0.5, step=0.01)]
-    max_leaf_nodes = [int(x) for x in np.arange(start=5, stop=200, step=5)]
-    max_leaf_nodes.append(None)
-    min_impurity_decrease = [x for x in np.arange(start=0.0, stop=1, step=0.01)]
-    bootstrap = [True, False]  # Create the random grid
-    random_grid = {'n_estimators': n_estimators,
-                   'max_features': max_features,
-                   'max_depth': max_depth,
-                   'min_samples_split': min_samples_split,
-                   'min_samples_leaf': min_samples_leaf,
-                   'bootstrap': bootstrap,
-                   'min_weight_fraction_leaf': min_weight_fraction_leaf,
-                   'max_leaf_nodes': max_leaf_nodes,
-                   'min_impurity_decrease': min_impurity_decrease,
-                   }
+    from sklearn.model_selection import RandomizedSearchCV
 
-    from sklearn.ensemble import RandomForestRegressor
-    #reg = RandomForestRegressor(random_state=42)
-    reg = RandomForestRegressor(n_estimators=231, min_weight_fraction_leaf=0.05,min_samples_split=2,
-                                min_samples_leaf=4, min_impurity_decrease=0.0, max_leaf_nodes=185,
-                                max_features='log2',max_depth=10,
-                                random_state=42, n_jobs=-1)
+    random_grid = {'n_iter': [x for x in np.arange(start=100, stop=1000, step=5)],
+                   'alpha_1': [x for x in np.arange(start=5.0, stop=50, step=0.1)],
+                   'alpha_2': [x for x in np.arange(start=0.0, stop=1, step=0.0001)],
+                   'lambda_1':[x for x in np.arange(start=50.0, stop=300, step=1)],
+                   'lambda_2':[x for x in np.arange(start=50.0, stop=300, step=1)],
+                   'fit_intercept': [True, False],
+                   'normalize': [True, False],
+                   'tol': [x for x in np.arange(start=5.0, stop=50, step=0.1)]}
+    #reg = linear_model.BayesianRidge()
 
-    # rf_random = RandomizedSearchCV(estimator=reg, param_distributions=random_grid, n_iter=2000, cv=3, verbose=2,random_state=42, n_jobs=-1)  # Fit the random search model
+    reg = linear_model.BayesianRidge(tol=18.79999999999995, normalize=True,
+                                     n_iter=245, lambda_2=191.0, lambda_1=240.0,
+                                     fit_intercept=True, alpha_2=0.026000000000000002,
+                                     alpha_1=13.39999999999997)
+
+    # rf_random = RandomizedSearchCV(estimator=reg, param_distributions=random_grid, n_iter=3000, cv=3, verbose=2,random_state=42, n_jobs=-1)  # Fit the random search model
     # rf_random.fit(train_data, train_labels)
     # print(rf_random.best_params_)
+
+
 
     reg.fit(train_data, train_labels)
 
     pred = reg.predict(test_data)
-    print("Forest Regressor")
-    print("RMSLE ", mean_squared_log_error(test_labels, pred))
-    print("RMSE ", mean_squared_error(test_labels, pred))
-    print("Variance ", explained_variance_score(test_labels, pred))
-    print("R2 ", r2_score(test_labels, pred))
+    print("Bayesian Ridge")
+    print("RMSLE ",mean_squared_log_error(test_labels, pred))
+    print("RMSE ",mean_squared_error(test_labels, pred))
+    print("Variance ",explained_variance_score(test_labels, pred))
 
-    #print("Validation Set")
-    #pred = reg.predict(valid_set_data)
-    #print(mean_squared_log_error(valid_set_labels, pred))
-    #print(explained_variance_score(valid_set_labels, pred))
 
     if pred_set is not None:
         v_pred = reg.predict(pred_set)
         v_pred = np.exp(v_pred)
         res = pd.DataFrame({"Id":pred_set_id,"SalePrice":v_pred})
-        res.to_csv("predictions.csv", index=False)
+        res.to_csv("predictionsRidge.csv", index=False)
 
+    return pred, v_pred
     ##########################################################################
+
 
 def ridge(train_data, train_labels, test_data, test_labels,
                      pred_set=None,pred_set_id=None):
@@ -172,7 +153,7 @@ def ridge(train_data, train_labels, test_data, test_labels,
     #                'solver': solver,
     #                'max_iter': max_iter,
     #                'tol': tol}
-
+    #reg = linear_model.Ridge()
     reg = linear_model.Ridge(tol=0.06, solver="svd", max_iter=4500, fit_intercept=True,
                              alpha=0.46, random_state=42)
 
@@ -189,19 +170,27 @@ def ridge(train_data, train_labels, test_data, test_labels,
     print("RMSLE ",mean_squared_log_error(test_labels, pred))
     print("RMSE ",mean_squared_error(test_labels, pred))
     print("Variance ",explained_variance_score(test_labels, pred))
-    print("R2 ", r2_score(test_labels,pred))
-    #print("Validation Set")
-    #pred = reg.predict(valid_set_data)
-    #print(mean_squared_log_error(valid_set_labels, pred))
-    #print(explained_variance_score(valid_set_labels, pred))
+
 
     if pred_set is not None:
         v_pred = reg.predict(pred_set)
         v_pred = np.exp(v_pred)
         res = pd.DataFrame({"Id":pred_set_id,"SalePrice":v_pred})
-        res.to_csv("predictions.csv", index=False)
+        res.to_csv("predictionsRidge.csv", index=False)
 
+    return pred, v_pred
     ##########################################################################
+
+
+def feature_engineering(d):
+    d['TotalSF'] = d['TotalBsmtSF'] + d['1stFlrSF'] + d['2ndFlrSF']
+
+    d['Bathrooms'] = d['BsmtFullBath'] + d['BsmtHalfBath'] + d['FullBath'] + d['HalfBath']
+    d['PorchSF'] = d['OpenPorchSF'] + d['EnclosedPorch'] + d['3SsnPorch'] + d['ScreenPorch']
+
+
+
+    return d
 
 
 def data_pipeline(d):
@@ -236,9 +225,9 @@ def data_pipeline(d):
         continue
 
     scale_log_attribs = ['LotFrontage','MasVnrArea','LotArea',
-                         '1stFlrSF','2ndFlrSF','BsmtFinSF1',
+                         '1stFlrSF','2ndFlrSF','BsmtFinSF1','BsmtFinSF2',
                          'BsmtUnfSF','GrLivArea',
-                         'OpenPorchSF','TotalBsmtSF','WoodDeckSF']
+                         'OpenPorchSF','TotalBsmtSF']
 
     for att in scale_log_attribs:
         try:
@@ -247,6 +236,7 @@ def data_pipeline(d):
         except KeyError:
             print("Warning, KeyError, attrib not found in data")
             continue
+
 
 
     factorization_attribs = list(d.select_dtypes(include=[np.object]).columns)
@@ -260,15 +250,22 @@ def data_pipeline(d):
             print("Warning, KeyError, attrib not found in data")
         continue
 
-
     return d
 
 
 def main():
+    dropped_attribs = ["Id", "Alley",
+                       "Street", 'PoolQC', 'Utilities', 'RoofStyle',
+                       'RoofMatl', "PoolArea", 'BsmtFinSF1', 'BsmtFinSF2', 'GarageQual',
+                        'Exterior2nd',
+                       '1stFlrSF', '2ndFlrSF',
+                       'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch',
+                       'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
 
-    from sklearn.model_selection import KFold
+                       ]
 
     train_data, test_data = train_test_split(data, test_size=0.3, random_state=42)
+
 
     train_labels = train_data['SalePrice'].apply(np.log)
     train_data = train_data.drop(columns='SalePrice')
@@ -278,12 +275,45 @@ def main():
     test_data = test_data.drop(columns='SalePrice')
 
     train_data = data_pipeline(train_data)
+    train_data = feature_engineering(train_data)
+
+    # for column in train_data:
+    #     plt.scatter(train_data[column], train_labels,  alpha=0.5)
+    #
+    #     plt.xlabel(column)
+    #     plt.ylabel('SalePrice')
+    #     fig = matplotlib.pyplot.gcf()
+    #     fig.set_size_inches(18.5, 10.5)
+    #     plt.savefig('plots/scatterplots/scatter_' + str(column) + '.png')
+    #     plt.close()
+    # exit()
+
     test_data = data_pipeline(test_data)
+    test_data = feature_engineering(test_data)
+
 
     pred_set = pd.read_csv('test.csv')
     pred_set_id = pred_set['Id']
-    pred_set = pred_set.drop(columns=dropped_attribs)
     pred_set = data_pipeline(pred_set)
+    pred_set = feature_engineering(pred_set)
+
+    train_data = train_data.drop(columns=dropped_attribs)
+    test_data = test_data.drop(columns=dropped_attribs)
+    pred_set = pred_set.drop(columns=dropped_attribs)
+
+
+    # import seaborn as sns
+    # for column in data:
+    #     try:
+    #         sns.distplot(data[column], color='blue', axlabel=column)
+    #         fig = matplotlib.pyplot.gcf()
+    #         fig.set_size_inches(18.5, 10.5)
+    #         plt.savefig('plots/histograms/hist2_' + str(column) + '.png',dpi=100)
+    #         plt.close()
+    #     except:
+    #         pass
+    #
+    # exit()
 
     while True:
         from sklearn import linear_model
@@ -291,7 +321,6 @@ def main():
         ols = linear_model.LinearRegression()
 
         ols = ols.fit(train_data, train_labels)
-
 
         xlabels = list(train_data.columns)
         statCoef = list(stats.coef_pval(ols, train_data,train_labels))
@@ -301,8 +330,8 @@ def main():
             if h < statCoef[x+1]:
                 h = statCoef[x+1]
 
-                #0.05
-        if h > 0.07:
+                #0.07
+        if h > 0.06:
             idx = statCoef.index(h) - 1
             train_data = train_data.drop(columns=xlabels[idx])
             test_data = test_data.drop(columns=xlabels[idx])
@@ -313,164 +342,38 @@ def main():
 
     stats.summary(ols, train_data, train_labels, xlabels=xlabels)
 
-    #forest_regressor(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
 
-    linear_reg(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
+    pred1, vpred1 = linear_reg(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
+    pred2, vpred2 = lasso(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
+    pred3, vpred3 = ridge(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
+    pred4, vpred4 = bayesian_ridge(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
 
-    lasso(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
+    pred = (pred1 + pred2 + pred3 + pred4) / 4
+    vpred = (vpred1 + vpred2 + vpred3 + vpred4) / 4
 
-    ridge(train_data, train_labels, test_data, test_labels,pred_set,pred_set_id)
+    print("Combined Prediction")
+    print("RMSLE ", mean_squared_log_error(test_labels, pred))
+    print("RMSE ", mean_squared_error(test_labels, pred))
+    print("Variance ", explained_variance_score(test_labels, pred))
 
+
+    res = pd.DataFrame({"Id": pred_set_id, "SalePrice": vpred})
+    res.to_csv("predictionsCombined.csv", index=False)
 
 data = pd.read_csv('iowaHomes.csv')
 
 
-from sklearn.neighbors import LocalOutlierFactor
-outliers = data.loc[data['SalePrice'] > 50000]
-outliers = outliers[['SalePrice']]
-outliers['outlier'] = LocalOutlierFactor(n_neighbors=100, contamination=0.1).fit_predict(outliers)
-outliers = outliers.loc[outliers['outlier'] == -1].index.values
-data = data.drop(outliers)
+data = data.drop(data[(data['OverallQual'] < 5.0) & (data['SalePrice'] > 200000)].index)
+data = data.drop(data[(data['OverallQual'] == 8.0) & (data['SalePrice'] > 470000)].index)
+data = data.drop(data[(data['OverallQual'] == 9.0) & (data['SalePrice'] > 430000)].index)
 
-outliers = data.loc[data['OverallQual'] > 3]
-outliers = outliers[['OverallQual','SalePrice']]
-outliers['outlier'] = LocalOutlierFactor(n_neighbors=150, contamination=0.1).fit_predict(outliers)
-outliers = outliers.loc[outliers['outlier'] == -1].index.values
-print(outliers)
-print(len(outliers))
-
-plt.scatter(data['OverallQual'], data['SalePrice'], alpha=0.2)
-plt.xlabel('OverallQual')
-plt.ylabel('SalePrice')
-fig = matplotlib.pyplot.gcf()
-fig.set_size_inches(18.5, 10.5)
-plt.savefig('plots/scatterplots/scatter_' + str('OverallQual') + '.png')
-plt.close()
-
-
-data = data.drop(outliers)
-
-
-#data = data_pipeline(data)
-# for column in data:
-#     plt.scatter(data[column], data['SalePrice'], alpha=0.5)
-#     plt.xlabel(column)
-#     plt.ylabel('SalePrice')
-#     fig = matplotlib.pyplot.gcf()
-#     fig.set_size_inches(18.5, 10.5)
-#     plt.savefig('plots/scatterplots/scatter_' + str(column) + '.png')
-#     plt.close()
-# exit()
-
-#data = data.drop(data[(data['SalePrice'] > 550000)].index)
-
-# data = data.drop(data[(data['OverallQual'] < 5.0) & (data['SalePrice'] > 200000)].index)
-# data = data.drop(data[(data['OverallQual'] == 8.0) & (data['SalePrice'] > 470000)].index)
-# data = data.drop(data[(data['OverallQual'] == 9.0) & (data['SalePrice'] > 430000)].index)
-# data = data.drop(data[(data['OverallQual'] == 7.0) & (data['SalePrice'] > 360000)].index)
-# data = data.drop(data[(data['OverallQual'] == 7.0) & (data['SalePrice'] < 90000)].index)
-
-
-data = data.drop(data[(data['YearBuilt'] < 1920) & (data['SalePrice'] > 250000)].index)
 data = data.drop(data[(data['YearBuilt'] < 1960) & (data['SalePrice'] > 300000)].index)
-
 
 data = data.drop(data[data['LotArea'] > 35000].index)
 
-
-
-data = data.drop(data[(data['OverallCond'] == 2.0) & (data['SalePrice'] > 330000)].index)
+data = data.drop(data[(data['SalePrice'] > 550000)].index)
 
 data = data.drop(data[(data['MSZoning'] == "RM") & (data['SalePrice'] > 300000)].index)
 
 
-#data = data.drop([341,348])
-#
-# plt.scatter(data['BsmtFinSF1'], data['SalePrice'], alpha=0.5)
-# plt.xlabel('BsmtFinSF1')
-# plt.ylabel('SalePrice')
-#plt.show()
-#exit()
-
-
-#
-
-
-#print(data['SalePrice'])
-#exit()
-
-dropped_attribs = ["Id", "Alley",
-                    "Street",'PoolQC','3SsnPorch',"2ndFlrSF",
-                   'EnclosedPorch','Utilities','RoofStyle',
-                   'RoofMatl',"PoolArea"
-                   ]
-
-
-data = data.drop(columns=dropped_attribs)
-
-
-#data = data.drop(columns=["GarageCars",'YearBuilt','1stFlrSF'])
-# import seaborn as sns
-# plt.figure(figsize=(12,10))
-# cor = data.corr()
-# sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
-# plt.show()
-# #Correlation with output variable
-# cor_target = abs(cor["SalePrice"])#Selecting highly correlated features
-# relevant_features = cor_target[cor_target>0.5]
-# print(relevant_features.sort_values(ascending=False))
-#
-# #print(data[["OverallQual","1stFlrSF"]].corr())
-# #print(data[["OverallQual","TotRmsAbvGrd"]].corr())
-# #print(data[["1stFlrSF","TotRmsAbvGrd"]].corr())
-#
-
-#OverallQual #1stFlrSF SalePrice
-
-
-#exit()
-#data, valid_set = train_test_split(data, test_size=0.2, random_state=42)
-
-# data = factorize_data(data)
-# data = data.drop(columns=['SalePrice'])
-# Q1 = data.quantile(0.25)
-# Q3 = data.quantile(0.75)
-# IQR = Q3 - Q1
-# print(IQR)
-# print(data.shape)
-# data = data[~((data < (Q1 - 1.5 * IQR)) |(data > (Q3 + 1.5 * IQR))).any(axis=1)]
-# print(data.shape)
-# exit()
-#
-
-#
-# for x in range(data.shape[1]):
-#     col = data.iloc[:, x]
-#     if len(col.unique()) < 15:
-#         print(col.value_counts())
-# print(data[:19])
-#
-# exit()
-
 main()
-
-# distributions = [
-#     ('Unscaled data', None),
-#     ('Data after min-max scaling',
-#      MinMaxScaler().fit_transform),
-#     ('Data after max-abs scaling',
-#      MaxAbsScaler().fit_transform),
-#     ('Data after robust scaling',
-#      RobustScaler(quantile_range=(25, 75)).fit_transform),
-#     ('Data after power transformation (Yeo-Johnson)',
-#      PowerTransformer(method='yeo-johnson').fit_transform),
-#     ('Data after power transformation (Box-Cox)',
-#      PowerTransformer().fit_transform),
-#     ('Data after quantile transformation (uniform pdf)',
-#      QuantileTransformer(output_distribution='uniform')
-#      .fit_transform),
-#     ('Data after sample-wise L2 normalizing',
-#      Normalizer().fit_transform),
-#     ('Data after standardization',
-#      StandardScaler().fit_transform)
-# ]
