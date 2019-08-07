cb592b4edb226b49329de7b57e451b7c67c0e513
Xymos <xymos0@gmail.com>
2019-08-07 16:36:34 +0100

    1

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ pd.set_option('display.width', 1000)
 pd.options.mode.chained_assignment = None
 
 
-def linear_reg(train_data, train_labels, test_data, test_labels):
+def linear_reg(train_data, train_labels, test_data, test_labels, pred_set=None, pred_set_id=None):
 
     reg = linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=-1,
                      normalize=False)
@@ -29,14 +29,58 @@ def linear_reg(train_data, train_labels, test_data, test_labels):
     filename = 'savedModels/l_reg_model.sav'
     pickle.dump(reg, open(filename, 'wb'))
 
+    args = {'MSZoning': 'RH',
+            'OverallQual': 5,
+            'OverallCond': 6,
+            'ExterCond': 'TA',
+            'YearBuilt': 961,
+            'YearRemodAdd': 1961,
+            'LotArea': 11622,
+            'GrLivArea': 896,
+            '1stFlrSF': 896,
+            '2ndFlrSF': 0,
+            'BsmtQual': 'TA',
+            'BsmtExposure': 'No',
+            'BsmtFinType1': 'Rec',
+            'BsmtFullBath': 0,
+            'TotalBsmtSF': 882,
+            'HeatingQC': 'TA',
+            'CentralAir': 'Y',
+            'BedroomAbvGr': 2,
+            'KitchenAbvGr': 1,
+            'KitchenQual': 'TA',
+            'Functional': 'Typ',
+            'Fireplaces': 0,
+            'GarageYrBlt': 1961,
+            'GarageCars': 1,
+            'GarageArea': 730,
+            'PavedDrive': 'Y',
+            'WoodDeckSF': 40,
+            'OpenPorchSF': 0,
+            'EnclosedPorch': 0,
+            '3SsnPorch': 0,
+            'ScreenPorch': 120,
+            'MasVnrType': 'None',
+            'MasVnrArea': 0}
+    #test_data = run_prediction(args)
 
-
-
+    #print(test_data)
+    #exit()
     pred = reg.predict(test_data)
-    print("Linear Regression")
-    print("RMSLE ", mean_squared_log_error(test_labels, pred))
-    print("RMSE ", mean_squared_error(test_labels, pred))
-    print("Variance ", explained_variance_score(test_labels, pred))
+    # print("Linear Regression")
+    # print("RMSLE ", mean_squared_log_error(test_labels, pred))
+    # print("RMSE ", mean_squared_error(test_labels, pred))
+    # print("Variance ", explained_variance_score(test_labels, pred))
+    #print(pred)
+    #print(np.exp(pred))
+
+
+    # v_pred = []
+    # if pred_set is not None:
+    #     v_pred = reg.predict(pred_set)
+    #     v_pred = np.exp(v_pred)
+    #     res = pd.DataFrame({"Id": pred_set_id, "SalePrice": v_pred})
+    #     res.to_csv("predictionsLinearReg.csv", index=False)
 
     return pred
 
@@ -76,11 +120,13 @@ def lasso(train_data, train_labels, test_data, test_labels):
     pickle.dump(reg, open(filename, 'wb'))
 
     pred = reg.predict(test_data)
-    print("Lasso")
-    print("RMSLE ", mean_squared_log_error(test_labels, pred))
-    print("RMSE ", mean_squared_error(test_labels, pred))
-    print("Variance ", explained_variance_score(test_labels, pred))
-
+    try:
+        print("Lasso")
+        print("RMSLE ", mean_squared_log_error(test_labels, pred))
+        print("RMSE ", mean_squared_error(test_labels, pred))
+        print("Variance ", explained_variance_score(test_labels, pred))
+    except:
+        pass
 
     return pred
 
@@ -116,10 +162,12 @@ def bayesian_ridge(train_data, train_labels, test_data, test_labels):
 
     pred = reg.predict(test_data)
     print("Bayesian Ridge")
-    print("RMSLE ",mean_squared_log_error(test_labels, pred))
-    print("RMSE ",mean_squared_error(test_labels, pred))
-    print("Variance ",explained_variance_score(test_labels, pred))
-
+    try:
+        print("RMSLE ",mean_squared_log_error(test_labels, pred))
+        print("RMSE ",mean_squared_error(test_labels, pred))
+        print("Variance ",explained_variance_score(test_labels, pred))
+    except:
+        pass
 
 
     return pred
@@ -154,9 +202,12 @@ def ridge(train_data, train_labels, test_data, test_labels):
 
     pred = reg.predict(test_data)
     print("Ridge")
-    print("RMSLE ",mean_squared_log_error(test_labels, pred))
-    print("RMSE ",mean_squared_error(test_labels, pred))
-    print("Variance ",explained_variance_score(test_labels, pred))
+    try:
+        print("RMSLE ",mean_squared_log_error(test_labels, pred))
+        print("RMSE ",mean_squared_error(test_labels, pred))
+        print("Variance ",explained_variance_score(test_labels, pred))
+    except:
+        pass
 
     return pred
     ##########################################################################
@@ -225,12 +276,17 @@ def encode_values(d):
         try:
             d[att] = enc.fit_transform(d[att].values.reshape(-1, 1))
         except:
+            print(att)
             continue
 
     return d
 
 
-def log_scale_values(d):
+def log_scale_values(d, training_data):
+
+    from sklearn.preprocessing import FunctionTransformer
+    transformer = FunctionTransformer(np.log1p, validate=True)
+    transformer.fit(training_data)
 
     scale_log_attribs = ['LotFrontage', 'MasVnrArea', 'LotArea',
                          '1stFlrSF', '2ndFlrSF', 'BsmtFinSF1', 'BsmtFinSF2',
@@ -239,15 +295,17 @@ def log_scale_values(d):
 
     for att in scale_log_attribs:
         try:
-            d[att] = d[att].astype(float).apply(np.log)
-            d[att] = d[att].astype(float).replace(-np.inf, 0)
+            d[att] = transformer.transform(d[att].values.reshape(-1, 1))
+
+            #d[att] = d[att].astype(float).apply(np.log)
+            #d[att] = d[att].astype(float).replace(-np.inf, 0)
         except:
             continue
 
     return d
 
 
-def drop_colums(d):
+def drop_columns(d):
     dropped_attribs = ["Alley",
                        "Street", 'PoolQC', 'Utilities', 'RoofStyle',
                        'RoofMatl', "PoolArea", 'BsmtFinSF1', 'BsmtFinSF2', 'GarageQual',
@@ -261,7 +319,7 @@ def drop_colums(d):
                        # estimate the price of a house based on these as the house hasn't been sold yet
 
                        ]
-    d = d.drop(columns=dropped_attribs)
+    d = d.drop(columns=dropped_attribs, errors="ignore")
 
     return d
 
@@ -270,6 +328,7 @@ def run_prediction(args):
 
     args_df = pd.DataFrame(columns=list(args.keys()))
     args_df.loc[0] = list(args.values())
+
 
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'savedModels/28features')
 
@@ -282,15 +341,21 @@ def run_prediction(args):
     args_df = args_df.reset_index(drop=True)
 
     args_df = feature_engineering(args_df)
-    args_df = drop_colums(args_df)
 
     args_df = args_df.dropna(axis=1)
 
     args_df = encode_values(args_df)
 
+    import copy
+    args_df_copy = copy.deepcopy(args_df)
 
-    args_df = log_scale_values(args_df)
+    args_df = log_scale_values(args_df, args_df_copy)
+
+
+    args_df = drop_columns(args_df)
     args_df = args_df[len(training_data):]
+
+    print(args_df)
 
     n_models = 0
     preds = 0
@@ -303,11 +368,14 @@ def run_prediction(args):
             n_models += 1
 
 
-    preds = preds / n_models
+    preds = preds[0] / n_models
+
+    preds = log_scale_values(preds, args_df_copy)
+
     print(preds)
-    print(np.exp(preds))
-    import math
-    print(math.exp(preds))
+    #print(np.exp(preds))
+    #import math
+    #print(math.exp(preds))
 
     return 0
 
@@ -335,6 +403,50 @@ def main():
 
     train_data, test_data = train_test_split(data, test_size=0.3, random_state=42)
 
+    args = {
+            'MSSubClass': 20.0,
+            'MSZoning': 'RL',
+            'OverallQual': 5,
+            'OverallCond': 6,
+            'BsmtUnfSF': 0.0,
+            'ExterCond': 'TA',
+            'GarageCond': 'TA',
+            'LotFrontage': 80.0,
+            'TotRmsAbvGrd': 7.0,
+            'YearBuilt': 1977,
+            'YearRemodAdd': 1977,
+            'LotArea': 12984,
+            'GrLivArea': 1647,
+            '1stFlrSF': 1647,
+            '2ndFlrSF': 0,
+            'BsmtQual': 'Gd',
+            'BsmtExposure': 'Mn',
+            'BsmtFinType1': 'ALQ',
+            'BsmtFullBath': 1,
+            'TotalBsmtSF': 1430,
+            'HeatingQC': 'Ex',
+            'CentralAir': 'Y',
+            'BedroomAbvGr': 3,
+            'KitchenAbvGr': 1,
+            'KitchenQual': 'Gd',
+            'Functional': 'Typ',
+            'Fireplaces': 1,
+            'GarageYrBlt': 1977,
+            'GarageCars': 2,
+            'GarageArea': 621,
+            'PavedDrive': 'Y',
+            'WoodDeckSF': 0,
+            'OpenPorchSF': 0,
+            'EnclosedPorch': 0,
+            '3SsnPorch': 0,
+            'ScreenPorch': 0,
+            'MasVnrType': 'BrkFace',
+            'MasVnrArea': 459}
+    args_df = pd.DataFrame(columns=list(args.keys()))
+    args_df.loc[0] = list(args.values())
+    test_data = pd.concat([test_data, args_df])
+    print(test_data.tail(n=2))
+
     train_labels = train_data['SalePrice'].apply(np.log)
     train_data = train_data.drop(columns='SalePrice')
 
@@ -345,25 +457,43 @@ def main():
     train_data = fill_missing_val(train_data)
     test_data = fill_missing_val(test_data)
 
-    train_test_data = pd.concat([train_data, test_data])
-    train_test_data.to_csv('savedModels/training_data.csv', index=False) #saving out data with no missing values to use when making predictions in production
 
 
     train_data = feature_engineering(train_data)
     test_data = feature_engineering(test_data)
 
+    train_test_data = pd.concat([train_data, test_data])  # data joined to perform factorization and scaling
 
-    train_test_data = pd.concat([train_data, test_data]) #data joined to perform factorization and scaling
 
-
-    train_test_data = drop_colums(train_test_data)
+    train_test_data.to_csv('savedModels/training_data.csv',
+                           index=False)  # saving out data with no missing values to use when making predictions in production
 
     train_test_data = encode_values(train_test_data)
-    train_test_data = log_scale_values(train_test_data)
 
+
+    train_test_data = log_scale_values(train_test_data, train_test_data)
+    train_test_data = drop_columns(train_test_data)
 
     test_data = train_test_data[len(train_data):]
     train_data = train_test_data[:len(train_data)]
+
+    #print(test_data.tail(n=2))
+    #exit()
+    # training_data = pd.read_csv(
+    #     os.path.join(os.path.dirname(os.path.abspath(__file__)), 'savedModels/training_data.csv'),
+    #     keep_default_na=False)
+
+    #
+    # pred_set = pd.read_csv('test.csv')
+    # pred_set_id = pred_set['Id']
+    # pred_set = pred_set.drop(columns=['Id'])
+    # pred_set = fill_missing_val(pred_set)
+    # pred_set = feature_engineering(pred_set)
+    # pred_set = encode_values(pred_set)
+    # pred_set = log_scale_values(pred_set, training_data)
+    #
+    # pred_set = drop_columns(pred_set)
+
 
     while True:
 
@@ -384,11 +514,14 @@ def main():
             train_data = train_data.drop(columns=xlabels[idx])
             test_data = test_data.drop(columns=xlabels[idx])
 
+            #pred_set = pred_set.drop(columns=xlabels[idx])
         else:
             break
 
     stats.summary(ols, train_data, train_labels, xlabels=xlabels)
 
+    print(test_data.tail(n=2))
+    #exit()
     pred1 = linear_reg(train_data, train_labels, test_data, test_labels)
     pred2 = lasso(train_data, train_labels, test_data, test_labels)
     pred3 = ridge(train_data, train_labels, test_data, test_labels)
@@ -397,53 +530,23 @@ def main():
 
     pred = (pred1 + pred2 + pred3 + pred4) / 4
 
-    print("Combined Prediction")
-    print("RMSLE ", mean_squared_log_error(test_labels, pred))
-    print("RMSE ", mean_squared_error(test_labels, pred))
-    print("Variance ", explained_variance_score(test_labels, pred))
+    # print("Combined Prediction")
+    # print("RMSLE ", mean_squared_log_error(test_labels, pred))
+    # print("RMSE ", mean_squared_error(test_labels, pred))
+    # print("Variance ", explained_variance_score(test_labels, pred))
 
-    print(pred)
+
+    print(np.exp(pred))
+
 
 if __name__ == "__main__":
-    args = {'MSZoning': 'C',
-            'OverallQual': '7',
-            'OverallCond': '5',
-            'ExterCond': 'TA',
-            'YearBuilt': '2003',
-            'YearRemodAdd': '2003',
-            'LotArea': '8450',
-            'GrLivArea': '1710',
-            '1stFlrSF': '856',
-            '2ndFlrSF': '854',
-            'BsmtQual': 'Gd',
-            'BsmtExposure': 'No',
-            'BsmtFinType1': 'GLQ',
-            'BsmtFullBath': '1',
-            'TotalBsmtSF': '856',
-            'HeatingQC': 'Ex',
-            'CentralAir': 'Y',
-            'BedroomAbvGr': '3',
-            'KitchenAbvGr': '1',
-            'KitchenQual': 'Gd',
-            'Functional': 'Typ',
-            'Fireplaces': '0',
-            'GarageYrBlt': '2003',
-            'GarageCars': '2',
-            'GarageArea': '548',
-            'PavedDrive': 'Y',
-            'WoodDeckSF': '0',
-            'OpenPorchSF': '61',
-            'EnclosedPorch': '0',
-            '3SsnPorch': '0',
-            'ScreenPorch': '0',
-            'MasVnrType': 'BrkFace',
-            'MasVnrArea': '196'}
+
 
     #pr = cProfile.Profile()
     #pr.enable()
 
-    #main()
+    main()
 
     #pr.disable()
     #pr.print_stats(sort='time')
-    print(run_prediction(args))
+    #print(run_prediction(args))
